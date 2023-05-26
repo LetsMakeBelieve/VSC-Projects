f89d63edf4668d4027722afae80dc58b22dbbd06
LetsMakeBelieve <Mitchell2karan@gmail.com>
2023-05-25 18:26:45 -0700

    Added Fibonacci sequence

--- LearningGit/index.js
+++ LearningGit/index.js
@@ -1,2 +1,17 @@
 console.log("Watch this:\n")
-//display a fibonacci sequence with spaces between each number
+
+function fibonacci(n) {
+    var sequence = [0, 1]; // Initialize the sequence with the first two numbers
+  
+    for (var i = 2; i < n; i++) {
+      var num = sequence[i - 1] + sequence[i - 2]; // Calculate the next number in the sequence
+      sequence.push(num); // Add the number to the sequence
+    }
+  
+    return sequence;
+  }
+  
+  // Testing the function
+  var n = 10; // Change this value to generate a different number of Fibonacci numbers
+  var result = fibonacci(n);
+  console.log(result);
